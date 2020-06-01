595dd4ec51f3707ead3ab0a6a5efc50c529c145c
mateusschwede <mateus1908.schwede@gmail.com>
2020-06-01 12:39:06 -0300

    Login finalizado

--- index.php
+++ index.php
@@ -1,14 +1,53 @@
 <?php
     require_once 'conect.php';
+    $msg = null;
+
+    if((!empty($_POST['nome'])) and (!empty($_POST['senha']))) {
+        $r = $db->prepare("SELECT id FROM cliente WHERE nome=? AND senha=?");
+        $r->execute(array($_POST['nome'],$_POST['senha']));
+        $r2 = $db->prepare("SELECT id FROM tecnico WHERE nome=? AND senha=?");
+        $r2->execute(array($_POST['nome'],$_POST['senha']));
+        if($r->rowCount()>0) {
+            session_start();
+            $_SESSION['nome'] = $_POST['nome'];
+            $_SESSION['senha'] = $_POST['senha'];
+            $_SESSION['msgm'] = null;
+            header("location: pCliente.php");
+        } elseif($r2->rowCount()>0) {
+            session_start();
+            $_SESSION['nome'] = $_POST['nome'];
+            $_SESSION['senha'] = $_POST['senha'];
+            $_SESSION['msgm'] = null;
+            header("location: pTecnico.php");
+        } elseif(($_POST['nome']=="admin") and ($_POST['senha']=="111")) {
+            session_start();
+            $_SESSION['nome'] = $_POST['nome'];
+            $_SESSION['senha'] = $_POST['senha'];
+            $_SESSION['msgm'] = null;
+            header("location: pAdmin.php");
+        } else {$msg = "<br><div class='alert alert-danger alert-dismissible fade show' role='alert'>Dado(s) incorreto(s)!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";}
+    }
 ?>
 
-<body>
+<body id="entrada">
 <div class="container-fluid">
 
 
     <div class="row">
         <div class="col-sm-12">
-            Login aqui
+            <img src="https://img.icons8.com/color/100/000000/technical-support.png">
+            <h1>EChamado</h1>
+            <h5>Software gestor de suporte tecnico</h5>
+            <form action="index.php" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" required name="nome" maxlength="50" placeholder="Nome" style="text-transform: lowercase;">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" required name="senha" maxlength="5" placeholder="Senha" style="text-transform: lowercase;">
+                </div>
+                <button type="submit" class="btn btn-secondary">Entrar</button>
+            </form>
+            <?php if($msg!=null){echo$msg;$msg=null;}?>
         </div>
     </div>
 
---
+++ offline.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="shortcut icon" href="https://img.icons8.com/color/48/000000/technical-support.png">
+    <title>EChamado</title>
+</head>
+<body id="entrada">
+<div class="container-fluid">
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <h3><svg class="bi bi-exclamation-circle text-danger" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/></svg><br>Houve algum problema com a conexão!</h3>
+            <h5>Tente novamente em breve</h5>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='index.php'">Recarregar</button>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>
