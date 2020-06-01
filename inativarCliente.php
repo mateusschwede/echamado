ebb838ec64dd48d2624786774982155a4d150c31
mateusschwede <mateus1908.schwede@gmail.com>
2020-06-01 16:09:54 -0300

    inativarMaquina e cliente finalizados/atualizados

--- inativarCliente.php
+++ inativarCliente.php
@@ -4,13 +4,13 @@
     if((empty($_SESSION['nome'])) or (empty($_SESSION['senha']))) {header("location: index.php");}
 
     if(!empty($_GET['id'])) {
-        $r = $db->prepare("SELECT id FROM chamado WHERE idCliente=? AND situacao='andamento'");
+        $r = $db->prepare("SELECT id FROM chamado WHERE idCliente=? AND (situacao='andamento' OR situacao='pendente')");
         $r->execute(array(base64_decode($_GET['id'])));
         if($r->rowCount()==0) {
             $r = $db->prepare("UPDATE cliente SET ativo=0,ipMaquina=null WHERE id=?");
             $r->execute(array(base64_decode($_GET['id'])));
             $_SESSION['msgm'] = "<br><div class='alert alert-success alert-dismissible fade show' role='alert'>Cliente id ".base64_decode($_GET['id'])." inativado, máquina disponível!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><br>";
             header("location: admCliente.php");
-        } else {$_SESSION['msgm'] = "<br><div class='alert alert-danger alert-dismissible fade show' role='alert'>Negado: cliente com chamado em andamento!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><br>";}
+        } else {$_SESSION['msgm'] = "<br><div class='alert alert-danger alert-dismissible fade show' role='alert'>Negado: cliente com chamado em andamento!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><br>"; header("location: admCliente.php");}
     } else {header("location: index.php");}
 ?>
---
+++ inativarMaquina.php
@@ -0,0 +1,18 @@
+<?php
+    require_once 'conect.php';
+    session_start();
+    if((empty($_SESSION['nome'])) or (empty($_SESSION['senha']))) {header("location: index.php");}
+
+    if(!empty($_GET['ip'])) {
+        $r = $db->prepare("SELECT ipMaquina FROM chamado WHERE ipMaquina=? AND (situacao='andamento' OR situacao='pendente')");
+        $r->execute(array(base64_decode($_GET['ip'])));
+        if($r->rowCount()==0) {
+            $r = $db->prepare("UPDATE cliente SET ipMaquina=null WHERE ipMaquina=?");
+            $r->execute(array(base64_decode($_GET['ip'])));
+            $r = $db->prepare("UPDATE maquina SET ativo=0 WHERE ip=?");
+            $r->execute(array(base64_decode($_GET['ip'])));
+            $_SESSION['msgm'] = "<br><div class='alert alert-success alert-dismissible fade show' role='alert'>Máquina IP ".base64_decode($_GET['ip'])." inativada, cliente sem máquina!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><br>";
+            header("location: admMaquina.php");
+        } else {$_SESSION['msgm'] = "<br><div class='alert alert-danger alert-dismissible fade show' role='alert'>Negado: máquina com chamado em andamento!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><br>"; header("location: admMaquina.php");}
+    } else {header("location: index.php");}
+?>
