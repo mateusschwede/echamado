cd10f186876443fc3aa08bf24f93be7e6482db0f
mateusschwede <mateus1908.schwede@gmail.com>
2020-06-02 16:27:39 -0300

    menu pTecnico atualizado

--- pTecnico.php
+++ pTecnico.php
@@ -16,7 +16,7 @@
     <div class="row">
         <div class="col-sm-12" id="menu">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="pAdmin.php"><img src="https://img.icons8.com/color/48/000000/technical-support.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> EChamado</a>
+                <a class="navbar-brand" href="pTecnico.php"><img src="https://img.icons8.com/color/48/000000/technical-support.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> EChamado</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
