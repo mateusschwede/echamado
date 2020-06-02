31957b6fc6476dc636a9e6333cb0b6cd282b1d1c
mateusschwede <mateus1908.schwede@gmail.com>
2020-06-02 10:02:02 -0300

    admRelData finalizado

--- admRelData.php
+++ admRelData.php
@@ -22,8 +22,8 @@
                         <li class="nav-item dropdown active">
                             <a class="nav-link dropdown-toggle" href="#" id="relatorios" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Relatórios</a>
                             <div class="dropdown-menu" aria-labelledby="relatorios">
-                                <a class="dropdown-item active" href="admRelCliente.php">Chamados por cliente</a>
-                                <a class="dropdown-item" href="admRelData.php">Chamados por data</a>
+                                <a class="dropdown-item" href="admRelCliente.php">Chamados por cliente</a>
+                                <a class="dropdown-item active" href="admRelData.php">Chamados por data</a>
                                 <a class="dropdown-item" href="admRelMaquina.php">Chamados por máquina</a>
                                 <a class="dropdown-item" href="admRelTecnico.php">Chamados por tecnico</a>
                             </div>
@@ -37,20 +37,10 @@
 
     <div class="row">
         <div class="col-sm-12">
-            <h1>Chamados por cliente</h1>
-            <form action="admRelCliente.php" method="post">
+            <h1>Chamados por data</h1>
+            <form action="admRelData.php" method="post">
                 <div class="form-group">
-                    <label for="selectCliente">Cliente</label>
-                    <select class="form-control" required name="idCliente" id="selectCliente">
-                        <?php
-                            $r = $db->query("SELECT id,nome,ativo FROM cliente ORDER BY nome,ativo");
-                            $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
-                            foreach($linhas as $l) {
-                                if($l['ativo']==0) {echo "<option value=".$l['id'].">".$l['id']."- ".$l['nome']." (inativo)</option>";}
-                                else {echo "<option value=".$l['id'].">".$l['id']."- ".$l['nome']."</option>";}
-                            }
-                        ?>
-                    </select>
+                    <input type="date" required name="dia" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-success">Verificar</button>
             </form>
@@ -61,34 +51,34 @@
         <div class="col-sm-12">
             <br>
             <?php
-                if(!empty($_POST['idCliente'])) {
-                    $r = $db->prepare("SELECT * FROM chamado WHERE idCliente=? ORDER BY dthrCadastro DESC,situacao,tipo DESC");
-                    $r->execute(array($_POST['idCliente']));
-                    $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
-                    foreach($linhas as $l) {
-                        switch ($l['situacao']) {
-                            case "pendente":
-                                $borda = "item2";
-                                break;
-                            case "andamento":
-                                $borda = "item3";
-                                break;
-                            default:
-                                $borda = "item1";
-                                break;
-                        }
-                        switch ($l['tipo']) {
-                            case "leve":
-                                $cor = "#28A745";
-                                break;
-                            case "moderado":
-                                $cor = "#FFC107";
-                                break;
-                            default:
-                                $cor = "#DC3545";
-                                break;
-                        }
-                        echo "
+            if(!empty($_POST['dia'])) {
+                $r = $db->prepare("SELECT * FROM chamado WHERE DATE(dthrCadastro)=? ORDER BY dthrCadastro DESC,situacao,tipo DESC");
+                $r->execute(array($_POST['dia']));
+                $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
+                foreach($linhas as $l) {
+                    switch ($l['situacao']) {
+                        case "pendente":
+                            $borda = "item2";
+                            break;
+                        case "andamento":
+                            $borda = "item3";
+                            break;
+                        default:
+                            $borda = "item1";
+                            break;
+                    }
+                    switch ($l['tipo']) {
+                        case "leve":
+                            $cor = "#28A745";
+                            break;
+                        case "moderado":
+                            $cor = "#FFC107";
+                            break;
+                        default:
+                            $cor = "#DC3545";
+                            break;
+                    }
+                    echo "
                             <li class='list-group-item' id=".$borda.">
                                 <div class='d-flex w-100 justify-content-between'>
                                     <h5 class='mb-1'>Chamado ".$l['id']."</h5>
@@ -97,36 +87,36 @@
                                 <p class='mb-1'>Tipo: <span style='color: ".$cor."'><b>".$l['tipo']."</b></span></p>
                                 <p class='mb-1'>Situação: ".$l['situacao']."</p>
                         ";
-                        $r2 = $db->prepare("SELECT nome FROM maquina WHERE ip=?");
-                        $r2->execute(array($l['ipMaquina']));
-                        $linhas2 = $r2->fetchAll(PDO::FETCH_ASSOC);
-                        foreach($linhas2 as $l2) {$nomeMaquina = $l2['nome'];}
-                        echo "
+                    $r2 = $db->prepare("SELECT nome FROM maquina WHERE ip=?");
+                    $r2->execute(array($l['ipMaquina']));
+                    $linhas2 = $r2->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($linhas2 as $l2) {$nomeMaquina = $l2['nome'];}
+                    echo "
                                 <p class='mb-1'>Máquina: (".$l['ipMaquina'].") ".$nomeMaquina."</p>
                         ";
-                        $r3 = $db->prepare("SELECT nome FROM tecnico WHERE id=?");
-                        $r3->execute(array($l['idTecnico']));
-                        $linhas3 = $r3->fetchAll(PDO::FETCH_ASSOC);
-                        foreach($linhas3 as $l3) {$nomeTecnico = $l3['nome'];}
-                        echo "
+                    $r3 = $db->prepare("SELECT nome FROM tecnico WHERE id=?");
+                    $r3->execute(array($l['idTecnico']));
+                    $linhas3 = $r3->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($linhas3 as $l3) {$nomeTecnico = $l3['nome'];}
+                    echo "
                                 <p class='mb-1'>Técnico: (".$l['idTecnico'].") ".$nomeTecnico."</p>
                         ";
-                        $r4 = $db->prepare("SELECT nome FROM cliente WHERE id=?");
-                        $r4->execute(array($l['idCliente']));
-                        $linhas4 = $r4->fetchAll(PDO::FETCH_ASSOC);
-                        foreach($linhas4 as $l4) {$nomeCliente = $l4['nome'];}
-                        echo "
+                    $r4 = $db->prepare("SELECT nome FROM cliente WHERE id=?");
+                    $r4->execute(array($l['idCliente']));
+                    $linhas4 = $r4->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($linhas4 as $l4) {$nomeCliente = $l4['nome'];}
+                    echo "
                                 <p class='mb-1'>Cliente: (".$l['idCliente'].") ".$nomeCliente."</p>
                         ";
-                        if($l['dthrAnalise']!=null) {echo "<p class='mb-1'>Análise: ".$l['dthrAnalise']."</p>";}
-                        if($l['dthrFinalizado']!=null) {echo "<p class='mb-1'>Finalizado: ".$l['dthrFinalizado']."</p>";}
-                        echo "
+                    if($l['dthrAnalise']!=null) {echo "<p class='mb-1'>Análise: ".$l['dthrAnalise']."</p>";}
+                    if($l['dthrFinalizado']!=null) {echo "<p class='mb-1'>Finalizado: ".$l['dthrFinalizado']."</p>";}
+                    echo "
                                 <p class='mb-1'>Descrição: ".$l['descricao']."</p>
                             </li>
                         ";
-                        echo "<br>";
-                    }
+                    echo "<br>";
                 }
+            }
             ?>
         </div>
     </div>
